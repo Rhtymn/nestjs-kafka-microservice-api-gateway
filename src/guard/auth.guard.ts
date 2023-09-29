009dd0721bf3505e5b32bad1a702a3deada78c53
Roihan <hroihan6@gmail.com>
2023-09-29 19:52:06 +0700

    Modify auth guard for handling authorization

--- src/guard/auth.guard.ts
+++ src/guard/auth.guard.ts
@@ -7,7 +7,7 @@ import {
 import { AuthService } from '../auth/service/auth.service';
 import { ValidateTokenResponse } from '@app/interface/auth';
 import { Reflector } from '@nestjs/core';
-import { IS_PUBLIC_KEY } from '@app/constants';
+import { IS_PUBLIC_KEY, ROLES_KEY } from '@app/constants';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -17,31 +17,48 @@ export class AuthGuard implements CanActivate {
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
+    const isPublic: boolean | undefined = this.getMetadata<boolean>(
+      context,
+      IS_PUBLIC_KEY,
+    );
+    const roles: string[] | undefined = this.getMetadata<string[]>(
+      context,
+      ROLES_KEY,
+    );
+
     if (isPublic) {
       return true;
     }
 
-    const request = context.switchToHttp().getRequest();
-    const token: string | undefined | null =
-      request.headers?.authorization?.split(' ')[1];
+    if (roles && roles.length > 0) {
+      const request = context.switchToHttp().getRequest();
+      const token: string | undefined | null =
+        request.headers?.authorization?.split(' ')[1];
 
-    if (!token) {
-      return false;
-    }
+      if (!token) {
+        return false;
+      }
 
-    const isValid: ValidateTokenResponse = await this.authService.validateToken(
-      { token },
-    );
+      const isValid: ValidateTokenResponse =
+        await this.authService.validateToken({ token });
 
-    if (isValid.status === HttpStatus.OK) {
-      request.user = isValid.payload;
-      return true;
+      if (
+        isValid.status === HttpStatus.OK &&
+        roles.includes(isValid.payload.role)
+      ) {
+        request.user = isValid.payload;
+        return true;
+      }
     }
 
     return false;
+  }
+
+  // Create generic function to get metadata from reflector
+  private getMetadata<T>(context: ExecutionContext, key: string): T {
+    return this.reflector.getAllAndOverride<T>(key, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
   }
 }
